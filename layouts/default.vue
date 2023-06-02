026f0ff9fb1f25c981e36c349e584d9d0e90929e
Den <denis.inozemtsev@btb.dev>
2023-06-02 23:02:54 +0400

    add tailwind. init pages

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex flex-col justify-center items-center h-screen">
+        <nuxt-page />
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex flex-col">
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    </div>
+</template>
